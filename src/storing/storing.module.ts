19148ce79f7f15733b52a33442be2e0fb78d11d9
oleg.miniuk <oleg.miniuk@gmail.com>
2020-09-03 17:02:37 +0300

    storing service mock

--- src/storing/storing.module.ts
+++ src/storing/storing.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StoringController } from './storing.controller';
 import { StoringService } from './storing.service';
+import { EncryptionService } from '../encryption/encryption.service';
+
 
 @Module({
   controllers: [StoringController],
-  providers: [StoringService]
+  providers: [StoringService, EncryptionService]
 })
 export class StoringModule {}
--- src/storing/storing.service.ts
+++ src/storing/storing.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { StoredData } from './interfaces/stored-data.interface';
+import { EncryptionService } from '../encryption/encryption.service';
 
 @Injectable()
 export class StoringService {
+  constructor(private encryptionService: EncryptionService) {
+  }
+
   storeData(storeDataDto): string {
-    console.log(storeDataDto);
+    const {encryption_key, value} = storeDataDto;
+    const encryptedData = this.encryptionService.encryptData(value, encryption_key);
+    //TODO save encrypted data to db
+    console.log(encryptedData);
     return 'ok';
   }
 
   getData(getDataDto): StoredData {
     console.log(getDataDto);
-    return {id: "13", value: {}};
+    const {decryption_key, id} = getDataDto;
+    //TODO get data from DB
+    const decryptedData = this.encryptionService.decryptData('', decryption_key);
+    return {id, value: decryptedData};
   }
 }
